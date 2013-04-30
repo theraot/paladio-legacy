784511df5151b58bd5c8c61201df6bc8c0aa51d6
Alfonso J. Ramos <theraot@gmail.com>
2013-04-30 18:57:32 -0500

    documentation and bug fixes

--- paladio/session.lib.php
+++ paladio/session.lib.php
@@ -5,12 +5,24 @@
 		exit();
 	}
 
+	/**
+	 * Session
+	 * @package Paladio
+	 */
 	final class Session
 	{
 		//------------------------------------------------------------
 		// Public (Class)
 		//------------------------------------------------------------
 
+		/**
+		 * Clears the values of the current session.
+		 *
+		 * @param $commit: if set to true calls session_commit();
+		 *
+		 * @acess public
+		 * @return void
+		 */
 		public static function Clear($commit = true)
 		{
 			$keys = array_keys($_SESSION);
@@ -24,6 +36,14 @@
 			}
 		}
 
+		/**
+		 * Starts a new session if no previous session exists.
+		 *
+		 * Returns true if no previous session existed, false otherwise.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function Start()
 		{
 			if (!Session::Exists() && !headers_sent())
@@ -37,11 +57,25 @@
 			}
 		}
 
+		/**
+		 * Destroys the current session.
+		 *
+		 * @acess public
+		 * @return void
+		 */
 		public static function Destroy()
 		{
 			session_destroy();
 		}
 
+		/**
+		 * Verifies if a session currently exists.
+		 *
+		 * Returns true if a session currently exists, false otherwise.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function Exists()
 		{
 			if (session_id() != '')
@@ -56,6 +90,14 @@
 
 		//------------------------------------------------------------
 
+		/**
+		 * Clears the tokens associated with this session.
+		 *
+		 * Note: the tokens are stored in $_SESSION['tokens'].
+		 *
+		 * @acess public
+		 * @return void
+		 */
 		public static function ClearTokens()
 		{
 			if (Session::Exists())
@@ -64,6 +106,18 @@
 			}
 		}
 
+		/**
+		 * Creates a new token and stores it in this session.
+		 *
+		 * Returns the new token if a session currently exists, false otherwise.
+		 *
+		 * Note: the tokens are stored in $_SESSION['tokens'].
+		 *
+		 * @param $length: the length of the new token.
+		 *
+		 * @acess public
+		 * @return mixed
+		 */
 		public static function CreateToken($length = 10)
 		{
 			if (Session::Exists())
@@ -85,6 +139,18 @@
 			}
 		}
 
+		/**
+		 * Consumes a token from this session.
+		 *
+		 * Returns true if a session currently exists and the token was consumed, false otherwise.
+		 *
+		 * Note: the tokens are stored in $_SESSION['tokens'].
+		 *
+		 * @param $token: the token to consume.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function ClaimToken($token)
 		{
 			if (Session::Exists())
@@ -120,6 +186,18 @@
 
 		//------------------------------------------------------------
 
+		/**
+		 * Adds a value to the queue of this session.
+		 *
+		 * Returns true if a session currently exists, false otherwise.
+		 *
+		 * Note: the queue is stored in $_SESSION['queue'].
+		 *
+		 * @param $data: the value to add to the queue.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function Enqueue($data)
 		{
 			if (Session::Exists())
@@ -140,6 +218,18 @@
 			}
 		}
 
+		/**
+		 * Attempts to retrieve a value from the queue of this session.
+		 *
+		 * Returns true if a session currently exists and the value was retrieved, false otherwise.
+		 *
+		 * Note: the queue is stored in $_SESSION['queue'].
+		 *
+		 * @param &$data: set to the value retrieved from the queue.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function TryDequeue(&$data)
 		{
 			if (Session::Exists())
@@ -155,14 +245,24 @@
 				}
 			}
 		}
-		
+
+		/**
+		 * Retrieves all the values from the queue of this session.
+		 *
+		 * Returns the entire queue if a session currently exists, false otherwise.
+		 *
+		 * Note: the queue is stored in $_SESSION['queue'].
+		 *
+		 * @acess public
+		 * @return mixed
+		 */
 		public static function DequeueAll()
 		{
 			if (Session::Exists())
 			{
 				if (!array_key_exists('queue', $_SESSION) || !is_array($_SESSION['queue']))
 				{
-					return false;
+					return array();
 				}
 				else
 				{
@@ -179,6 +279,18 @@
 
 		//------------------------------------------------------------
 
+		/**
+		 * Gets the status of this session identified by the key $status.
+		 *
+		 * Returns the status if a session currently exists and the status has been set, false otherwise.
+		 *
+		 * Note: the status is stored in $_SESSION['status'].
+		 *
+		 * @param $status: the name of the status to retrieve.
+		 *
+		 * @acess public
+		 * @return mixed
+		 */
 		public static function get_Status($status)
 		{
 			if (Session::Exists())
@@ -202,6 +314,19 @@
 			}
 		}
 
+		/**
+		 * Sets the status of this session identified by the key $status.
+		 *
+		 * Returns true a session currently exists, false otherwise.
+		 *
+		 * Note: the status is stored in $_SESSION['status'].
+		 *
+		 * @param $status: the name of the status to set.
+		 * @param $value: the value to store in the status identified by the key $status.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function set_Status($status, $value)
 		{
 			if (Session::Exists())
@@ -222,6 +347,18 @@
 			}
 		}
 
+		/**
+		 * Verifies if the status of this session identified by the key $status has been set.
+		 *
+		 * Returns true a session currently exists and the status has been set, false otherwise.
+		 *
+		 * Note: the status is stored in $_SESSION['status'].
+		 *
+		 * @param $status: the name of the status to verify.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function isset_Status($status)
 		{
 			if (Session::Exists())
@@ -245,6 +382,18 @@
 			}
 		}
 
+		/**
+		 * Unsets the value of the status of this session identified by the key $status.
+		 *
+		 * Returns true a session currently exists, false otherwise.
+		 *
+		 * Note: the status is stored in $_SESSION['status'].
+		 *
+		 * @param $status: the name of the status to unset.
+		 *
+		 * @acess public
+		 * @return bool
+		 */
 		public static function unset_Status($status)
 		{
 			if (Session::Exists())
@@ -273,6 +422,9 @@
 		// Public (Constructor)
 		//------------------------------------------------------------
 
+		/**
+		 * Creating instances of this class is not allowed.
+		 */
 		public function __construct()
 		{
 			throw new Exception('Creating instances of '.__CLASS__.' is forbidden');

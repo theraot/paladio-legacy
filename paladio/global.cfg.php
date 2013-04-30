ba1f1463768371567f61a89af32e044e298c50cb
Alfonso J. Ramos <theraot@gmail.com>
2013-04-30 15:53:04 -0500

    Moved database configuration to global.cfg.php

--- paladio/global.cfg.php
+++ paladio/global.cfg.php
@@ -1,15 +1,22 @@
 <?php header('HTTP/1.0 404 Not Found'); exit(); //Not to be seen ?>
 
-# Ésta es configuración global
+# [paladio-database] # Configuración para acceder a la base de datos
+# server = localhost
+# port =
+# database =
+# user =
+# password =
+# query_user =
+# query_password =
 
-[paladio]
+[paladio] #Configuración global
 timezone = America/Bogota
 
-[paladio-paths]
+[paladio-paths] #Configuración de rutas de acceso
 entities = ../files/entities
 themes = ../files/themes
 plugins = /plugins
 
-[paladio-strings]
+[paladio-strings] #Configuración de cadenas de texto
 weekdays = Domingo, Lunes, Martes, Miércoles, Jueves, Viernes, Sábado
 months = Enero, Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto, Septiembre, Noviembre, Diciembre

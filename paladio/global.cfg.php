d9cbc3035181fc8a56f38134a0538696ffa62d7c
Alfonso J. Ramos <theraot@gmail.com>
2013-05-25 17:49:25 -0500

    Addded missing Octubre... oops... >.<

--- paladio/global.cfg.php
+++ paladio/global.cfg.php
@@ -19,4 +19,4 @@ plugins = /plugins
 
 [paladio-strings] #Configuración de cadenas de texto
 weekdays = [Domingo, Lunes, Martes, Miércoles, Jueves, Viernes, Sábado]
-months = [Enero, Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto, Septiembre, Noviembre, Diciembre]
+months = [Enero, Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto, Septiembre, Octubre, Noviembre, Diciembre]

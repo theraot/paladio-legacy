6cd3e192475a47f2adcceb038165c86eb8cda787
Alfonso J. Ramos <theraot@gmail.com>
2013-10-17 04:10:44 -0500

    virtual menu options are now possible
    It is possible now to have menu options that are not linked to any url
    as long as they have childs. In order to do so, set menu-parent to an
    array describing the parent menu item.

--- paladio/plugins/AccessControl/menu.pet.php
+++ paladio/plugins/AccessControl/menu.pet.php
@@ -6,9 +6,9 @@
 	}
 	$validUris = AccessControl::ValidUris();
 	$keys = array_keys($validUris);
-	$selectedClass = isset($_ELEMENT['attributes']['selected-class']) ? $_ELEMENT['attributes']['selected-class'] : false;
-	$itemClass = isset($_ELEMENT['attributes']['item-class']) ? $_ELEMENT['attributes']['item-class'] : false;
-	$activeClass = isset($_ELEMENT['attributes']['active-class']) ? $_ELEMENT['attributes']['active-class'] : false;
+	$selectedClass = array_key_exists('selected-class', $_ELEMENT['attributes']) ? $_ELEMENT['attributes']['selected-class'] : false;
+	$itemClass = array_key_exists('item-class', $_ELEMENT['attributes']) ? $_ELEMENT['attributes']['item-class'] : false;
+	$activeClass = array_key_exists('active-class', $_ELEMENT['attributes']) ? $_ELEMENT['attributes']['active-class'] : false;
 	$source = $_ELEMENT['source'];
 	$path = $_ELEMENT['path']; 
 	if ($_ELEMENT['query'] !== '')
@@ -21,48 +21,71 @@
 	{
 		$entry = &$validUris[$key];
 		$entry['_link'] = $path.'/'.$key;
-		if (isset($entry['menu-title']))
+		if (array_key_exists('menu-title', $entry))
 		{
 			if ($entry['path'] == $source)
 			{
 				$entry['selected'] = true;
 			}
-			if (isset($entry['menu-parent']))
+			if (array_key_exists('menu-parent', $entry))
 			{
+				$virtual = 0;
 				$parentId = $entry['menu-parent'];
+				$array = array();
+				if (is_array($parentId))
+				{
+					$virtual = 1;
+					$array = array_merge($array, $parentId);
+					if (array_key_exists('menu-id', $parentId))
+					{
+						$parentId = $parentId['menu-id'];
+						$entry['menu-parent'] = $parentId;
+					}
+				}
 				if (!array_key_exists($parentId, $data))
 				{
-					$data[$parentId] = array('_childs' => array());
-				}
-				else if (!array_key_exists('_childs', $data[$parentId]))
-				{
-					$data[$parentId]['_childs'] = array();
-				}
-				if (isset($entry['menu-id']))
-				{
+					$data[$parentId] = $array;
+					if ($virtual == 1)
+					{
+						$virtual = 2;
+					}
 					$data[$parentId]['_childs'][] = &$entry;
-					$data[$entry['menu-id']] = &$entry;
 				}
 				else
 				{
-					$data[$parentId]['_childs'][] = &$entry;
+					if (array_key_exists('_childs', $data[$parentId]))
+					{
+						$data[$parentId]['_childs'][] = &$entry;
+					}
+					else
+					{
+						$data[$parentId]['_childs'] = array(&$entry);
+					}
+					$data[$parentId] = array_merge($array, $data[$parentId]);
+				}
+				if (array_key_exists('menu-id', $entry))
+				{
+					$data[$entry['menu-id']] = &$entry;
 				}
 				if (array_key_exists('selected', $entry) || array_key_exists('active', $entry))
 				{
 					$data[$parentId]['active'] = true;
 				}
+				if ($virtual == 2)
+				{
+					$tree[] = &$data[$parentId];
+				}
 			}
 			else
 			{
 				$tree[] = &$entry;
-				if (isset($entry['menu-id']))
+				if (array_key_exists('menu-id', $entry))
 				{
 					$data[$entry['menu-id']] = &$entry;
 				}
 			}
 		}
 	}
-	
 	if (!function_exists("EmitPaladioNavMenu"))
 	{
 		function __EmitPaladioNavMenu($class, $itemClass, $selectedClass, $activeClass, $entries)
@@ -97,26 +120,26 @@
 					echo implode(' ', $classes);
 					echo '"';
 				}
-				if (isset($entry['menu-id']))
+				if (array_key_exists('menu-id', $entry))
 				{
 					echo ' id="'.$entry['menu-id'].'"';
 				}
 				echo '><a ';
-				if (isset($entry['_link']))
+				if (array_key_exists('_link', $entry))
 				{
 					echo 'href="'.$entry['_link'].'"';
 				}
-				if (isset($entry['menu-target']))
+				if (array_key_exists('menu-target', $entry))
 				{
 					echo 'target="'.$entry['menu-target'].'"';
 				}
 				echo '>';
-				if (isset($entry['menu-title']))
+				if (array_key_exists('menu-title', $entry))
 				{
 					echo $entry['menu-title'];
 				}
 				echo '</a>';
-				if (isset($entry['_childs']))
+				if (array_key_exists('_childs', $entry))
 				{
 					__EmitPaladioNavMenu(null, $itemClass, $selectedClass, $activeClass, $entry['_childs']);
 				}
@@ -127,7 +150,7 @@
 	}
 	
 	echo '<nav>';
-	if (isset($_ELEMENT['attributes']['class']))
+	if (array_key_exists('class', $_ELEMENT['attributes']))
 	{
 		__EmitPaladioNavMenu($_ELEMENT['attributes']['class'], $itemClass, $selectedClass, $activeClass, $tree);
 	}

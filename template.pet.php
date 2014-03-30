4171b53913312d9953c1b7b82ba97e4e9f12ce5c
= <theraot@gmail.com>
2014-03-30 17:21:54 -0500

    template.pet.php improved to use recent changes
    - template.pet.php is now using PET_Utility::BuildAttributeString and
    PET_Utility::InvokePET more effectively
    - removed support for optional menu and changing menu position via
    attributes

--- template.pet.php
+++ template.pet.php
@@ -5,55 +5,22 @@
 		exit();
 	}
 	header('Content-Type: text/html; charset=UTF-8');
-	if (array_key_exists('menu', $_ELEMENT['attributes']))
-	{
-		$__menu = $_ELEMENT['attributes']['menu'];
-	}
-	else
-	{
-		$__menu = '';
-	}
 ?><!DOCTYPE html>
-<html <?php
-	if(isset($_ELEMENT['attributes']['lang']))
-	{
-		echo 'lang = "'.$_ELEMENT['attributes']['lang'].'"';
-	}
-	?>>
+<html <?php echo PET_Utility::BuildAttributesString($_ELEMENT['attributes'], 'lang') ?>>
 	<head>
 		<meta charset="UTF-8" />
-		<title><?php
-			if(isset($_ELEMENT['attributes']['title']))
-			{
-				echo $_ELEMENT['attributes']['title'];
-				if (Configuration::TryGet('paladio', 'sitename', $sitename))
-				{
-					echo ' - '.$sitename;
-				}
-			}
-		?></title><?php
-			if(isset($_ELEMENT['attributes']['stylesheets']))
-			{
-				$stylesheets = array_map('trim', explode(';', $_ELEMENT['attributes']['stylesheets']));
-				PaladioTheme::RequestStyleSheet($stylesheets);
-			}
+		<?php
+			echo PET_Utility::InvokePET('title', Utility::ArrayTake($_ELEMENT['attributes'], 'title'));
+			@PaladioTheme::RequestStyleSheet($_ELEMENT['attributes']['stylesheets']);
+			@PaladioTheme::RequestScript($_ELEMENT['attributes']['scripts']);
 			PaladioTheme::EmitStyleSheets();
-			if(isset($_ELEMENT['attributes']['scripts']))
-			{
-				$scripts = array_map('trim', explode(';', $_ELEMENT['attributes']['scripts']));
-				PaladioTheme::RequestScript($scripts);
-			}
 			PaladioTheme::EmitScripts();
 		?>
 	</head>
 	<body>
 		<div id="__wrap">
-			<@header><?php
-				if ($__menu == 'header')
-				{
-					echo '<@menu id="__menu" class="menu" selected-class="selected" active-class="selected"/>';
-				}
-			?></@header>
+			<@header/>
+			<?php echo PET_Utility::InvokePET('menu', array('id' => '__menu', 'class' => 'menu', 'selected-class' => 'selected', 'active-class' => 'selected')); ?>
 			<div id="__main"><?php if (Session::TryDequeue($message))
 					{
 						echo '<div id="__message" class="'.$message['type'].'">';
@@ -65,20 +32,9 @@
 				?><div id="__content">
 					<?php echo $_ELEMENT['contents']; ?>
 				</div>
-				<@aside><?php
-					if ($__menu == 'aside')
-					{
-						echo '<@menu id="__menu" class="menu" selected-class="selected" active-class="selected"/>';
-					}
-				?></@aside>
+				<@aside/>
 			</div>
-			<hr/>
-			<@footer><?php
-				if ($__menu == 'footer')
-				{
-					echo '<@menu id="__menu" class="menu" selected-class="selected" active-class="selected"/>';
-				}
-			?></@footer>
+			<@footer/>
 		</div>
 	</body>
 </html>

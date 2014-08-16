f32dfab4a2e675761b4fe2c3514b6f83b56dfc82
= <theraot@gmail.com>
2014-08-15 23:22:50 -0500

    Migrated access control configuration to new scheme
    Ditto as global configuration

---
+++ paladio/plugins/AccessControl/acesscontrol.cfg.lib.php
@@ -0,0 +1,27 @@
+<?php
+	if (count(get_included_files()) == 1)
+	{
+		header('HTTP/1.0 404 Not Found');
+		exit();
+	}
+	else
+	{
+		require_once('configuration.lib.php');
+	}
+	Configuration::Add
+	(
+		array
+		(
+			//Configuración para control de acceso
+			'paladio-accesscontrol' => array
+			(
+				// 'table =>
+				// 'id_field' =>
+				// 'password_field' =>
+				// 'salt_field' =>
+				// 'role_field' =>
+				// 'hash_algorithm' => sha1
+			)
+		)
+	);
+?>
